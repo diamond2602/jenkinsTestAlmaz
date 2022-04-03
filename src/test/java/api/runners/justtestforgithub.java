ae2c2259709a70f050400e7d210306898224a1dc
diamond2602 <diamond2602@gmail.com>
2022-04-03 13:34:15 -0700

    1jenkinstestcommit

--- src/test/java/api/runners/justtestforgithub.java
+++ src/test/java/api/runners/justtestforgithub.java
@@ -3,5 +3,9 @@ package api.runners;
 public class justtestforgithub {
     public static void main(String[] args) {
         System.out.println("just to test github");
+        System.out.println("just to test2 github");
+        System.out.println("just to test3 github");
+
+
     }
 }
